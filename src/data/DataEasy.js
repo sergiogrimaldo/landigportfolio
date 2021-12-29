dde66f1e4ff94c0f7dc990d01e7497121abb187c
Sergio Grimaldo <47435264+sergiogrimaldo@users.noreply.github.com>
2021-12-29 16:33:25 -0500

    data

--- src/data/DataEasy.js
+++ src/data/DataEasy.js
@@ -45,22 +45,288 @@ export const dataeasy = [
     },
     {
       id: 3,
-      question: "Who played the character of harry potter in movie?",
+      question: "This Canadian television sportscaster is known for his Hockey Night in Canada role, a commentary show during hockey games.",
       answers: [
         {
-          text: "Johnny Deep",
+          text: "Don McKellar",
           correct: false,
         },
         {
-          text: "Leonardo Di Caprio",
+          text: "Don Taylor",
           correct: false,
         },
         {
-          text: "Denzel Washington",
+          text: "Donald Sutherland",
           correct: false,
         },
         {
-          text: "Daniel Red Cliff",
+          text: "Don Cherry",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Who is the primary lyricist for Canadian progressive rock band Rush?",
+      answers: [
+        {
+          text: "Geddy Lee",
+          correct: false,
+        },
+        {
+          text: "Neil Peart",
+          correct: true,
+        },
+        {
+          text: "Alex Lifeson",
+          correct: false,
+        },
+        {
+          text: "John Rutsey",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Which team has won the most Stanley Cups in the NHL?",
+      answers: [
+        {
+          text: "Chicago Blackhawks",
+          correct: false,
+        },
+        {
+          text: "Toronto Maple Leafs",
+          correct: false,
+        },
+        {
+          text: "Montreal Canadians",
+          correct: true,
+        },
+        {
+          text: "Detroit Red Wings",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Which country does the power metal band Sabaton originate from?",
+      answers: [
+        {
+          text: "Germany",
+          correct: false,
+        },
+        {
+          text: "Sweden",
+          correct: true,
+        },
+        {
+          text: "United States",
+          correct: false,
+        },
+        {
+          text: "Finland",
+          correct: false,
+        },
+      ],
+    },
+    {
+      
+      //ac
+      id: 7,
+      question: "Which Japanese company is the world's largest manufacturer of motorcycles?",
+      answers: [
+        {
+          text: "Honda",
+          correct: true,
+        },
+        {
+          text: "Kawasaki",
+          correct: false,
+        },
+        {
+          text: "Suzuki",
+          correct: false,
+        },
+        {
+          text: "Yamaha",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Which of the following is NOT a Russian car manufacturer?",
+      answers: [
+        {
+          text: "Silant",
+          correct: false,
+        },
+        {
+          text: "Dragon",
+          correct: true,
+        },
+        {
+          text: "BYD",
+          correct: false,
+        },
+        {
+          text: "GAZ",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "In the web-comic Homestuck, what is the name of the game the 4 kids play?",
+      answers: [
+        {
+          text: "Husslie",
+          correct: false,
+        },
+        {
+          text: "Sburb",
+          correct: true,
+        },
+        {
+          text: "Homesick",
+          correct: false,
+        },
+        {
+          text: "Hiveswap",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "In the 1969 Cartoon show Dastardly and Muttley in Their Flying Machines, which were NOT one of the lyrics in the opening theme?",
+      answers: [
+        {
+          text: "Nab Him",
+          correct: false,
+        },
+        {
+          text: "Jab Him",
+          correct: false,
+        },
+        {
+          text: "Tab Him",
+          correct: false,
+        },
+        {
+          text: "Stab Him",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Benny, Brain, Fancy-Fancy, Spook and Choo-Choo were known associates of what Hanna Barbera cartoon character?",
+      answers: [
+        {
+          text: "Top Cat",
+          correct: true,
+        },
+        {
+          text: "Scooby-Doo",
+          correct: false,
+        },
+        {
+          text: "Snagglepuss",
+          correct: false,
+        },
+        {
+          text: "Yogi Bear",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Talos, the mythical giant bronze man, was the protector of which island?",
+      answers: [
+        {
+          text: "Sardinia",
+          correct: false,
+        },
+        {
+          text: "Crete",
+          correct: true,
+        },
+        {
+          text: "Sicily",
+          correct: false,
+        },
+        {
+          text: "Cyprus",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "In Norse mythology, what is the name of the serpent which eats the roots of the ash tree Yggdrasil?",
+      answers: [
+        {
+          text: "Nidhogg",
+          correct: true,
+        },
+        {
+          text: "Bragi",
+          correct: false,
+        },
+        {
+          text: "Odin",
+          correct: false,
+        },
+        {
+          text: "Ymir",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Nidhogg is a mythical creature from what mythology?",
+      answers: [
+        {
+          text: "Hindu",
+          correct: false,
+        },
+        {
+          text: "Norse",
+          correct: true,
+        },
+        {
+          text: "Egyptian",
+          correct: false,
+        },
+        {
+          text: "Greek",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "What immense structure is referred to in Norse Mythology as the Yggdrasil.",
+      answers: [
+        {
+          text: "Mountain",
+          correct: false,
+        },
+        {
+          text: "Castle",
+          correct: false,
+        },
+        {
+          text: "Temple",
+          correct: false,
+        },
+        {
+          text: "Tree",
           correct: true,
         },
       ],
